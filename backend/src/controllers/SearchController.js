1905de71bcac0137a62ae96f9e9b433488d55c46
Gilvam Mourão <gilvamm@gmail.com>
2020-02-14 21:02:10 -0300

    search case sensitive

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -5,13 +5,14 @@ module.exports = {
 
   // buscar todos os devs num raio de 10km e filtrar por tecnologias
   async index(request, response) {
-    console.log('request.query: ', request.query);
-
     const { latitude, longitude, techs } = request.query;
+    // const techsArray = parseStringAsArray(techs);
 
-    const techsArray = parseStringAsArray(techs);
 
-    const devs = await Dev.find({
+    const techsArray = parseStringAsArray(techs)
+      .map(item => new RegExp(item, 'i')); // case sensitive for mongoDb
+
+    const objToSearch = {
       techs: {
         $in: techsArray
       },
@@ -21,14 +22,18 @@ module.exports = {
             type: 'Point',
             coordinates: [longitude, latitude],
           },
-          $maxDistance: 10000, // distância máxima de 10km
+          $maxDistance: 100000000, // distância máxima de 10km
           // $minDistance: 1
         }
       }
-    });
+    };
 
+    if (!techsArray.length) { // remove filtro por techs caso não exista nada no array
+      delete objToSearch.techs;
+    }
 
-    return response.json({ devs });
+    const devs = await Dev.find(objToSearch);
+    return response.json(devs);
   },
 
   async update() {
